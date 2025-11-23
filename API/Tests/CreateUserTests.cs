eb4e8b4b5eeaf3032d06f774b95a6e851ce3e98b
SugotDev <m.p.slugocki@gmail.com>
2025-11-23 16:58:40 +0100

    Install Allure Report and set up raport fields

--- API/Tests/CreateUserTests.cs
+++ API/Tests/CreateUserTests.cs
@@ -1,17 +1,22 @@
-﻿using API.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Allure.Net.Commons;
+using Allure.NUnit;
+using Allure.NUnit.Attributes;
+using API.Models;
 using System.Net;
-using System.Text;
 using System.Text.Json;
-using System.Threading.Tasks;
 
 namespace API.Tests
 {
+    [AllureNUnit]
     public class CreateUserTests : BaseApiTest
     {
         [Test]
+        [AllureTag("API")]
+        [AllureSeverity(SeverityLevel.critical)]
+        [AllureOwner("Michał Sługocki")]
+        [AllureSuite("User Endpoints")]
+        [AllureSubSuite("Positive Tests")]
+        [AllureFeature("Create New User")]
         public async Task CreateUser()
         {
             //Arrange
@@ -22,17 +27,38 @@ namespace API.Tests
             var createdUser = JsonSerializer.Deserialize<CreateUserResponse>(postResponse.Content!, _serializerOptions);
 
             //Assert
-            Assert.That(postResponse.StatusCode, Is.EqualTo(HttpStatusCode.Created));
-            Assert.That(createdUser.Id, Is.Not.Null.And.Not.Empty);
-            Assert.That(createdUser.Email, Is.EqualTo(user.Email));
-            Assert.That(createdUser.FirstName, Is.EqualTo(user.FirstName));
-            Assert.That(createdUser.LastName, Is.EqualTo(user.LastName));
-            Assert.That(createdUser.CreatedAt, Is.Not.EqualTo(default(DateTime)));
-            Assert.That(createdUser.CreatedAt, Is.InRange(
-                DateTime.UtcNow.AddMinutes(-1),
-                DateTime.UtcNow.AddMinutes(1)
-            ));
-
+            await AllureApi.Step("Verify Status Code", async () =>
+            {
+                Assert.That(postResponse.StatusCode, Is.EqualTo(HttpStatusCode.Created));
+            });
+            await AllureApi.Step("Verify Create User Response", async () =>
+            {
+                Assert.That(postResponse.Content, Is.Not.Null.And.Not.Empty);
+            });
+            await AllureApi.Step("Verify Created User Id Exist", async () =>
+            {
+                Assert.That(createdUser.Id, Is.Not.Null.And.Not.Empty);
+            });
+            await AllureApi.Step("Verify Created User Email", async () =>
+            {
+                Assert.That(createdUser.Email, Is.EqualTo(user.Email));
+            });
+            await AllureApi.Step("Verify Created User First Name", async () =>
+            {
+                Assert.That(createdUser.FirstName, Is.EqualTo(user.FirstName));
+            });
+            await AllureApi.Step("Verify Created User Last Name", async () =>
+            {
+                Assert.That(createdUser.LastName, Is.EqualTo(user.LastName));
+            });
+            await AllureApi.Step("Verify Created User Date", async () =>
+            {
+                Assert.That(createdUser.CreatedAt, Is.Not.EqualTo(default(DateTime)));
+                Assert.That(createdUser.CreatedAt, Is.InRange(
+                    DateTime.UtcNow.AddMinutes(-1),
+                    DateTime.UtcNow.AddMinutes(1)
+                    ));
+            });
         }
     }
 }
--- API/Tests/GetUserTests.cs
+++ API/Tests/GetUserTests.cs
@@ -1,12 +1,22 @@
-﻿using API.Models;
+﻿using Allure.Net.Commons;
+using Allure.NUnit;
+using Allure.NUnit.Attributes;
+using API.Models;
 using System.Net;
 using System.Text.Json;
 
 namespace API.Tests
 {
+    [AllureNUnit]
     public class GetUserTests : BaseApiTest
     {
         [Test]
+        [AllureTag("API")]
+        [AllureSeverity(SeverityLevel.critical)]
+        [AllureOwner("Michał Sługocki")]
+        [AllureSuite("User Endpoints")]
+        [AllureSubSuite("Positive Tests")]
+        [AllureFeature("Get User By Id")]
         public async Task GetUserById()
         {
             //Arrange
@@ -21,15 +31,37 @@ namespace API.Tests
             var actualUser = JsonSerializer.Deserialize<UserResponse>(getResponse.Content!, _serializerOptions);
 
             //Assert
-            Assert.Multiple(() =>
+            Assert.Multiple(async () =>
             {
-                Assert.That(getResponse.StatusCode, Is.EqualTo(HttpStatusCode.OK));
-                Assert.That(getResponse.Content, Is.Not.Null.And.Not.Empty);
-                Assert.That(actualUser.Data.Id, Is.EqualTo(expectedUser.Data.Id));
-                Assert.That(actualUser.Data.Email, Is.EqualTo(expectedUser.Data.Email));
-                Assert.That(actualUser.Data.First_Name, Is.EqualTo(expectedUser.Data.First_Name));
-                Assert.That(actualUser.Data.Last_Name, Is.EqualTo(expectedUser.Data.Last_Name));
-                Assert.That(actualUser.Data.Avatar, Is.EqualTo(expectedUser.Data.Avatar));
+                await AllureApi.Step("Verify Status Code", async () =>
+                {
+                    Assert.That(getResponse.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+                });
+                await AllureApi.Step("Verify Content", async () =>
+                {
+                    Assert.That(getResponse.Content, Is.Not.Null.And.Not.Empty);
+                });
+                await AllureApi.Step("Verify User Id", async () =>
+                {
+                    Assert.That(actualUser.Data.Id, Is.EqualTo(expectedUser.Data.Id));
+                });
+                await AllureApi.Step("Verify User Email", async () =>
+                {
+                    Assert.That(actualUser.Data.Email, Is.EqualTo(expectedUser.Data.Email));
+                });
+                await AllureApi.Step("Verify User First Name", async () =>
+                {
+                    Assert.That(actualUser.Data.First_Name, Is.EqualTo(expectedUser.Data.First_Name));
+                });
+                await AllureApi.Step("Verify User Last Name", async () =>
+                {
+                    Assert.That(actualUser.Data.Last_Name, Is.EqualTo(expectedUser.Data.Last_Name));
+                });
+                await AllureApi.Step("Verify User Avatar", async () =>
+                {
+                    Assert.That(actualUser.Data.Avatar, Is.EqualTo(expectedUser.Data.Avatar));
+                });               
+                
             });
         }
     }
